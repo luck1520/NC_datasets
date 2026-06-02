c9d50966e9d4736ba7e7af24a8aaf3bc592553bd
haoyue <166774700+luck1520@users.noreply.github.com>
2026-06-02 22:51:57 +0800

    Delete contribution.R

--- contribution.R
+++
@@ -1,5 +0,0 @@
-pred_terms <- predict(gam_model, type = "terms")
-term_var <- apply(pred_terms, 2, var)
-total_var <- sum(term_var)
-variance_contributions <- (term_var / total_var) * 100
-variance_contributions
